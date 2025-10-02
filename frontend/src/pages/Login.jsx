c4b6f5f569f90070b4855cb9a06c761743d90ed9
Christabel Obi-Nwosu <157861550+Christabel091@users.noreply.github.com>
2025-10-02 00:49:51 -0500

    setup authentication pages #created login page create signup page create welcome and home page setup authcontext for auth as authprovider #display: run npm run dev after git pull/clone

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,52 @@
+/** @format */
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../components/AuthContext";
+import { Link } from "react-router-dom";
+const Login = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const response = await login(email, password);
+    if (response.user) navigate("/");
+    else setError(response.error);
+  };
+
+  return (
+    <div className="overlay">
+      <div className="auth-container">
+        <form onSubmit={handleLogin} className="auth-form">
+          <h2>Log in</h2>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="error-message">{error}</p>
+          <button className="btn-auth" type="Log in">
+            Login
+          </button>
+          <p className="link-text">
+            Don't have an account? <a href="/signup">Sign up</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
